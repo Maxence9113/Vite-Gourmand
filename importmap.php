5ae658ee1efaa58ab97571369873a61c9d6d6a3b
Maxence <contact@maxencedupuis.com>
2026-01-14 13:04:30 +0100

    frontend: Changement de design des pages pour etre conforme avec la maquette FIGMA.

--- importmap.php
+++ importmap.php
@@ -29,17 +29,4 @@ return [
     '@hotwired/turbo' => [
         'version' => '7.3.0',
     ],
-    'bootstrap' => [
-        'version' => '5.3.8',
-    ],
-    '@popperjs/core' => [
-        'version' => '2.11.8',
-    ],
-    'bootstrap/dist/css/bootstrap.min.css' => [
-        'version' => '5.3.8',
-        'type' => 'css',
-    ],
-    'feather-icons' => [
-        'version' => '4.29.2',
-    ],
 ];
